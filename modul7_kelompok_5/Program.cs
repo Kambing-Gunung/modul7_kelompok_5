40e518a90edda9c5fb0452a55c75521f84bbe94e
David NTB <davidntb7000@gmail.com>
2026-04-06 22:08:36 +0700

    Set this branch to default

--- modul7_kelompok_5/Program.cs
+++ modul7_kelompok_5/Program.cs
@@ -1,11 +1,1 @@
-﻿using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // DataMahasiswa1201230011.ReadJSON();
-        // TeamMembers1201230011.ReadJSON();
-        GlossaryItem1201230011.ReadJSON();
-    }
-}
+﻿Console.WriteLine("Hello World!");
